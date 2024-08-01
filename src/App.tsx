27c2ed5429041751c5fe8db191f6a18b47a8559d
Hiul <hiufailau.41@gmail.com>
2024-08-01 22:57:55 +0200

    install react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
 import React, { FC, ReactElement } from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
 
-import { customTheme } from './theme/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { customTheme } from './theme/customTheme';
 
+// Create a client
+const queryClient = new QueryClient();
 
-const App: FC = (): ReactElement => (
-  <ThemeProvider theme={customTheme}>
-    <CssBaseline />
-    <Dashboard />
-  </ThemeProvider>
-);
+const App: FC = (): ReactElement => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
